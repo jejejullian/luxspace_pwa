8ba81f09365cf0686fe5b1f5a5077a62533afee0
jejejullian <juliannurfadzlin659@gmail.com>
2023-09-17 23:14:53 +0700

    migrasi dari template ke komponen React

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer className="flex text-center px-4 py-8 justify-center">
+      <p className="text-sm">
+        2023 • All Rights Reserved LuxSpace by Julian
+      </p>
+    </footer>
+    )
+}
+
+export default Footer
